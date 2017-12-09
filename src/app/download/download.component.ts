6e9a09118a0f2e9a83f0bc7fe92c6ad8717b389f
Sebastian Dudas <sebastian.frontend@gmail.com>
2017-12-10 00:53:49 +0100

    Raspored sekcija promenjen

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -10,6 +10,9 @@ export class DownloadComponent implements OnInit {
   loremIpsum: string = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
   officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`;
 
+  uputstvoTitle: string = 'Uputstvo za instalaciju';
+  uputstvoSubtitle: string = 'Detaljan tutorijal za pocetnike';
+
   launcherName: string = 'Columbia State Launcher';
   launcherVersion: string = 'v1.0.0';
   launcherReleased: string = '30.12.2017';
