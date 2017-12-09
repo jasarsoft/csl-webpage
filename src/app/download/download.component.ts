dca9c73c5dafd7b3b82543aef04c9122cea1e188
Sebastian Dudas <sebastian.frontend@gmail.com>
2017-12-10 00:01:26 +0100

    Manje izmene + bindovani podaci
    Izmenjena pozadina footera, dodati podaci u download.component.ts i dodat link za download sa potvrdom skidanja

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloadComponent implements OnInit {
 
+  downloadUrl: string = 'https://columbia-state.com/launcher/setup-cs.exe';
+  downloaded: boolean = false;
+
+  fileSize: number = 28.7;
+  fileName: string = 'setup-cs.exe';
+
+  serverName: string = 'Columbia State';
+  serverSlogan: string = 'Neki slogan';
+
+  toastMsg: string;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  goToDownload(): void {
+    window.location.href=this.downloadUrl;
+    this.toggleDownloaded();
+  }
+
+  toggleDownloaded(): void {
+    this.downloaded = !this.downloaded;
+    this.toastMsg = 'Download fajla je upravo poceo!';
   }
 
 }
