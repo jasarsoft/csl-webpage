4864db5cedeb43d1b0f404bb43e9ee43c57b9787
Sebastian Dudas <sebastian.frontend@gmail.com>
2017-12-10 15:45:35 +0100

    Service
    Sredjen service

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServerInfoService } from '../server-info.service';
 
 
 @Component({
@@ -8,34 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloadComponent implements OnInit {
 
-  loremIpsum: string = 'Lorem ipsum text';
+  loremIpsum: string;
 
-  uputstvoTitle: string = 'Uputstvo za instalaciju';
-  uputstvoSubtitle: string = 'Detaljan tutorijal za pocetnike';
+  uputstvoTitle: string;
+  uputstvoSubtitle: string;
 
-  launcherName: string = 'Columbia State Launcher';
-  launcherVersion: string = 'v1.0.0';
-  launcherReleased: string = '30.12.2017';
+  launcherName: string;
+  launcherVersion: string;
+  launcherReleased: string;
 
-  serverText: string = `Ideja Columbia State je nastala inspirativnom pričom najvećeg dilera u povjesti kokaina Pablo Escobar-a iz kartela Medelin manifestovana i prikazana kroz seriju Narcos u prve dvije sezone. Istu su pokrenuli bivši članovi NovaMahala tima nakon što je projekat zaustavljen iz opravdanih razloga. Najbolja platforma za pretvorbu te ideje jednoglasno bio je San Andreas Multiplayer (SA-MP) preko kojeg zajedno sa igrom Grand Theft Auto San Andreas (GTA:SA) će tim pokušati implementirati jedno najveće uporište kriminala na spram vladajućeg zakonskog prava kojeg će nositi policijski odredi. Columbia State sadrži više dijelova koje je predstavljaju i to su server, launcher i forum. Columbia State Server je nosilac projekta Columbia State oko koje cijelokupna stvar teži. Server je isključivo čisti RolePlay tip sa elementima Hard Play-a i Real Life, koji se prožimaju kroz glavni tip i daju jačinu RP tipu. Glavni fokus je stavljen na biznise koji proizvode močne i dominantne ljude u gradu, mafije koje kroz svoje organizatorske vještine prave najveće probleme i najjačima te policijske snage za očuvanje reda i sprovedbu zakonski odredbi. Columbia State Launcher je aplikacija koja obezbeđuje sve potrebno igraču kako bih pristupio serveru pravilno i lako. Igrač je obavezan posjedovati launcher koji može da skine sa stranice.
-  
-  `;
+  serverText: string;
 
-  downloadUrl: string = 'https://columbia-state.com/launcher/setup-cs.exe';
+  downloadUrl: string;
   downloaded: boolean = false;
 
-  fileSize: number = 28.7;
-  fileName: string = 'setup-cs.exe';
+  fileSize: number;
+  fileName: string;
 
-  serverName: string = 'Columbia State';
-  serverSlogan: string = 'Neki slogan text ovde';
+  serverName: string;
+  serverSlogan: string;
 
   toastMsg: string;
 
-  constructor() { 
+  constructor(private _info: ServerInfoService) {
+   this.setInfo(_info);  
   }
 
   ngOnInit() {
+  }
+
+  setInfo(_info: ServerInfoService): void {
+    this.loremIpsum = _info.loremIpsum;
+    this.uputstvoTitle = _info.uputstvoTitle;
+    this.uputstvoSubtitle = _info.uputstvoSubtitle;
+    this.launcherName = _info.launcherName;
+    this.launcherVersion = _info.launcherVersion;
+    this.launcherReleased = _info.launcherReleased;
+    this.downloadUrl = _info.downloadUrl;
+    this.fileSize = _info.fileSize;
+    this.fileName = _info.fileName;
+    this.serverName = _info.serverName;
+    this.serverSlogan = _info.serverSlogan;
   }
 
   goToDownload(): void {
--- src/app/server-info.service.ts
+++ src/app/server-info.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ServerInfoService {
 
-  loremIpsum: string = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
-  officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`;
+  loremIpsum: string = `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+    officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`;
 
   uputstvoTitle: string = 'Uputstvo za instalaciju';
   uputstvoSubtitle: string = 'Detaljan tutorijal za pocetnike';
@@ -14,14 +14,13 @@ export class ServerInfoService {
   launcherReleased: string = '30.12.2017';
 
   downloadUrl: string = 'https://columbia-state.com/launcher/setup-cs.exe';
-  downloaded: boolean = false;
 
   fileSize: number = 28.7;
   fileName: string = 'setup-cs.exe';
 
   serverName: string = 'Columbia State';
   serverSlogan: string = 'Neki slogan';
-  
+
   constructor() { }
 
 }
