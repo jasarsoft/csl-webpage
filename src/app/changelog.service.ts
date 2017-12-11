d0775344afe4c53ae63b6b7f16fc196df9b6b47f
Sebastian Dudas <sebastian.frontend@gmail.com>
2017-12-11 12:54:50 +0100

    Zavrsen popup modal
    Changelog popup modal zavrsen, dodati text u changelog.service.ts

--- src/app/changelog.service.ts
+++ src/app/changelog.service.ts
@@ -4,10 +4,42 @@ import { Injectable } from '@angular/core';
 export class ChangelogService {
 
   changelog_list = [
-    'test1',
-    'test2',
-    'test3',
-    'test4'
+    { 
+      version: 'v1.0.0', 
+      released: '23.12.2017', 
+      log: [
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`, 
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`, 
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`
+      ]
+    },
+    { 
+      version: 'v2.0.0', 
+      released: '23.12.2017', 
+      log: [
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`, 
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`, 
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`
+      ]
+    },
+    { 
+      version: 'v3.0.0', 
+      released: '23.12.2017', 
+      log: [
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`, 
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`, 
+        `Lorem Ipsum, dolor sit amet consectetur adipisicing elit. Veritatis explicabo aliquam, saepe quod a voluptates
+        officiis ratione nihil. Placeat et, autem nulla dolore tempora eligendi animi quisquam blanditiis iusto earum?`
+      ]
+    }
   ];
 
   constructor() { }
--- src/app/download/modal-customclass.ts
+++ src/app/download/modal-customclass.ts
@@ -3,6 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 import { ChangelogService } from '../changelog.service';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector: 'ngbd-modal-customclass',
@@ -10,17 +11,22 @@ import { ChangelogService } from '../changelog.service';
     encapsulation: ViewEncapsulation.None,
     styleUrls: ['./modal-customclass.css']
 })
-export class NgbdModalCustomclass {
+export class NgbdModalCustomclass implements OnInit {
     closeResult: string;
 
     changeLog: any[];
 
-    constructor(private modalService: NgbModal, private _changelog: ChangelogService) {
-        this.changeLog = _changelog.getChangelog();
+    launcherVersion: string;
+    launcherReleased: string;
+
+    constructor(private modalService: NgbModal, private _changelogService: ChangelogService) {
      }
 
     open(content) {
         this.modalService.open(content, { windowClass: 'dark-modal' });
     }
 
+    ngOnInit(): void {
+        this.changeLog = this._changelogService.getChangelog();
+    }
 }
